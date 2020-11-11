7f72be549553f909b10ead5f6b3f3257a13a3480
Emir <40762681+emirgo@users.noreply.github.com>
2020-11-12 00:03:57 +0100

    Define name hotfix

--- include/main.h
+++ include/main.h
@@ -3,7 +3,7 @@
 
 #include "stm32f4xx_hal.h"
 
-#define LED_GREEN_OIN                   GPIO_PIN_5
+#define LED_GREEN_PIN                   GPIO_PIN_5
 #define LED_GREEN_PORT                  GPIOA
 #define LED_GREEN_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOA_CLK_ENABLE()
 
--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ int main(void) {
 
     while (1)
     {
-        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+        HAL_GPIO_TogglePin(LED_GREEN_PORT, LED_GREEN_PIN);
         HAL_Delay(1000);
     }
 }
@@ -32,11 +32,11 @@ int main(void) {
 void LED_GREEN_Init() {
     LED_GPIO_CLK_ENABLE();
     GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.Pin = LED_PIN;
+    GPIO_InitStruct.Pin = LED_GREEN_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-    HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+    HAL_GPIO_Init(LED_GREEN_PORT, &GPIO_InitStruct);
 }
 
 void SysTick_Handler(void) {
