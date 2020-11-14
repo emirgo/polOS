26ccc0c8845e2288f40fa78538e1962dc7395fd7
Emir <40762681+emirgo@users.noreply.github.com>
2020-11-14 22:44:05 +0100

    RCC clk enable & i2s init

--- src/pol_i2s.c
+++ src/pol_i2s.c
@@ -10,11 +10,13 @@
 
 void pol_i2s_gpio_init()
 {
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    __HAL_RCC_PWR_CLK_ENABLE();
     // enable clocks for
     // A, B, C ports
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 }
 
 void pol_i2s_dma_init()
@@ -44,4 +46,64 @@ void pol_i2s_init(void)
     {
         Error_Handler();
     }
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if(hi2s2.Instance==SPI2)
+    {
+        /* USER CODE BEGIN SPI2_MspInit 0 */
+
+        /* USER CODE END SPI2_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_SPI2_CLK_ENABLE();
+
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**I2S2 GPIO Configuration    
+        PC1     ------> I2S2_SD
+        PA6     ------> I2S2_MCK
+        PB10     ------> I2S2_CK
+        PB12     ------> I2S2_WS 
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_1;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Alternate = GPIO_AF7_SPI2;
+        HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin = GPIO_PIN_6;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Alternate = GPIO_AF6_SPI2;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_12;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        /* I2S2 DMA Init */
+        /* SPI2_TX Init */
+        hdma_spi2_tx.Instance = DMA1_Stream4;
+        hdma_spi2_tx.Init.Channel = DMA_CHANNEL_0;
+        hdma_spi2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+        hdma_spi2_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+        hdma_spi2_tx.Init.MemInc = DMA_MINC_ENABLE;
+        hdma_spi2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+        hdma_spi2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+        hdma_spi2_tx.Init.Mode = DMA_CIRCULAR;
+        hdma_spi2_tx.Init.Priority = DMA_PRIORITY_LOW;
+        hdma_spi2_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+        if (HAL_DMA_Init(&hdma_spi2_tx) != HAL_OK)
+        {
+            Error_Handler();
+        }
+    }
+
+    // issue to be resolved
+    __HAL_LINKDMA(&hi2s2,hdmatx,hdma_spi2_tx);
 }
